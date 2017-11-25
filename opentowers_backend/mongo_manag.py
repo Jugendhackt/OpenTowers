a9bf917ad9e5e4f3d019ebf224964e5b0344af0a
panda_no1 <tobias.hanf@hotmail.de>
2017-11-25 22:01:55 +0100

    update mongo_manag.py

--- opentowers_backend/mongo_manag.py
+++ opentowers_backend/mongo_manag.py
@@ -1,8 +1,13 @@
 import pprint
 import pymongo
+import triangulation 
 from pymongo import MongoClient
 
 def monge_connecten(json_input):
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 	client = MongoClient('localhost',27017);
 
 	cellid = json_input["cellid"]
@@ -14,6 +19,32 @@ def monge_connecten(json_input):
 		print("New cell tower added")
 	else:
 		client.OpenTower.post.update({"cellid":cellid},{"$push":{ "dataArray":data_Array}})
+<<<<<<< Updated upstream
 		pprint.pprint(client.OpenTower.post.find_one({"cellid": cellid }))
 		print("JSON: ", json_input)
 	print(json_input["dataArray"][0])
+=======
+		#pprint.pprint(client.OpenTower.post.find_one({"cellid": cellid }))
+
+	if client.OpenTower.post.find_one({"cellid":cellid, "dataArray.2":{"$exists":True}}) is None:
+		print("Not enought towers for triangulation")
+	else:
+		print("Triangulating new position")	
+		calc_location = pars_triangulation(client.OpenTower.post.find_one({"cellid":cellid}))
+		client.OpenTower.post.update({"cellid":cellid},{"$set":{"calc_Position":{"long":calc_location[1],"lati":calc_location[0]}}})	
+		
+
+def pars_triangulation(array_input):
+
+	long_g = []
+	lati_g = []
+	signal_Str = []
+
+	for i in range(len(array_input["dataArray"])):
+		long_g.append(array_input["dataArray"][i]["location"]["long"])
+		lati_g.append(array_input["dataArray"][i]["location"]["lati"])
+		signal_Str.append(array_input["dataArray"][i]["signal_Strength"])
+
+	return triangulation.triangulater(lati_g, long_g, signal_Str)
+	
+>>>>>>> Stashed changes
